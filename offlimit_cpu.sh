c976a0755a6811b419f1896644e0317b58602e59
Argus <richard.arthur003@stu.ucc.edu.gh>
2023-07-01 11:29:40 -0400

    boring Script

--- offlimit_cpu.sh
+++ offlimit_cpu.sh
@@ -5,33 +5,84 @@
 #40 - 1 -> Danger
 
 #Default
-COUNTER=5
-INTERVAL=2
+COUNTER=1
+INTERVAL=1
+ROW=2
+
 
 #List
 declare -a MY_ARRAY=()
 
-#regex 
-re="AAA \(bbb [[:digit:]]+\) CCC"
+function default_func {
+    my_command=$(sar -u 1 1 | grep -v "CPU" | awk 'NR==2 {if($9 < 100.00)print $9}')
+	if (( $(echo "$my_command" | awk '{print ($1 <= 100.00 && $1 >= 80.00)}') ));then
+		echo "CPU state is Excellent.:)";
+	elif (( $(echo "$my_command" | awk '{print ($1 < 80.00 && $1 >= 60.00)}') ));then
+		echo "CPU state is Moderate.:)";
+	elif (( $(echo "$my_command" | awk '{print ($1 < 60.00 && $1 >= 50.00)}') ));then
+		echo "CPU state is Normal.:)";
+	elif (( $(echo "$my_command" | awk '{print ($1 < 50.00 && $1 >= 30.00)}') ));then
+		echo "CPU Usage is advancing to a critical State. Close some program.:)"
+	elif (( $(echo "$my_command" | awk '{print ($1 < 30.00 && $1 >= 00.00)}') ));then
+		echo "Danger!!!"
+	fi
+}
 
-# #Asking for the input count for the processes
-# read -p "Enter the number of Count: " ANSWER_ONE
+function operation {
+	if [ $counter_cus -gt 1 ];then
+		my_custom=$(sar -u $interval_cus $counter_cus | grep -v "CPU" | awk 'NR==3 {if($9 < 100.00)print $9}')
+	else
+		my_custom=$(sar -u $interval_cus $counter_cus | grep -v "CPU" | awk 'NR==2 {if($9 < 100.00)print $9}')
+	fi
+}
 
-# #If conditon to start the operation
-# if ! [[$ANSWER_ONE =~ $re]];then
-# 	echo "Your entry is not a number. Pls it should a number.:)";
-# else
+function custom_func {
+	
+	operation
+
+	if (( $(echo "$my_custom" | awk '{print ($1 <= 100.00 && $1 >= 80.00)}') ));then
+		echo "CPU state is Excellent.:)";
+	elif (( $(echo "$my_custom" | awk '{print ($1 < 80.00 && $1 >= 60.00)}') ));then
+		echo "CPU state is Moderate.:)";
+	elif (( $(echo "$my_custom" | awk '{print ($1 < 60.00 && $1 >= 50.00)}') ));then
+		echo "CPU state is Normal.:)";
+	elif (( $(echo "$my_custom" | awk '{print ($1 < 50.00 && $1 >= 30.00)}') ));then
+		echo "CPU Usage is advancing to a critical State. Close some program.:)"
+	elif (( $(echo "$my_custom" | awk '{print ($1 < 30.00 && $1 >= 00.00)}') ));then
+		echo "Danger!!!"
+	fi
+}
+
+
 read -p "Enter Default[1] || Custom[2] : " ANSWER_ONE
 case "$ANSWER_ONE" in
 	[1])
+		default_func
+		;;
+	[2])
+		#Seti the counter and interval for the command
+		read -p "Enter Counter : " counter_cus
+		if ! [[ "$counter_cus" =~ ^[0-9]+$ ]];
+   		then
+		 	echo "error: Not a number";
+		else
+			echo "COUNTER = $counter_cus";
+			read -p "Enter Interval : " interval_cus
+			if ! [[ "$interval_cus" =~ ^[0-9]+$ ]]; 
+   			then
+		 		echo "error: Not a number";
+			else
+				echo "INTERVAL = $counter_cus";
+				custom_func $interval_cus $counter_cus
+			fi
+		fi
+
 		
-			MY_LIST=$(sar -u $INTERVAL $i | awk '{print $9}')
-			echo "$MY_LIST"
-			$MY_ARRAY+=($MY_LIST)
-		done
 		;;
 	*)
 		echo "[1]->Default or [2]->Custom. Number pls.:)";
 		;;
 esac
+
+
 
